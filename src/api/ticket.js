8f1a6bc37915b58620b48903f03082cb68eeaaff
GStarP <1700066935@qq.com>
2020-12-05 21:42:23 +0800

    feat: merge  并优化购票记录

--- src/api/ticket.js
+++ src/api/ticket.js
@@ -8,30 +8,22 @@ export async function getScheduleInfo(scheduleId) {
 }
 
 export async function getAllTicket(userId) {
-  const { data } = await axios.get(
-    `/ticket/get/${userId}`
-  );
+  const { data } = await axios.get(`/ticket/get/${userId}`);
   return data;
 }
 
 export async function issueTicket(ticketId) {
-  const { data } = await axios.get(
-    `/ticket/issue?ticketId=${ticketId}`
-  );
+  const { data } = await axios.get(`/ticket/issue?ticketId=${ticketId}`);
   return data;
 }
 
 export async function deleteTicket(ticketId) {
-  const { data } = await axios.get(
-    `/ticket/delete?ticketId=${ticketId}`
-  );
+  const { data } = await axios.get(`/ticket/delete?ticketId=${ticketId}`);
   return data;
 }
 
 export async function cancelTicket(ticketId) {
-  const { data } = await axios.get(
-    `/ticket/cancel?ticketId=${ticketId}`
-  );
+  const { data } = await axios.get(`/ticket/cancel?ticketId=${ticketId}`);
   return data;
 }
 
